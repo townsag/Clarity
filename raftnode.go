140374564dd56b0b6f4b19a9a939e9042fd17898
HarrisonHwang <78999886+HarrisonHwang@users.noreply.github.com>
2024-11-06 00:49:39 -0800

    Add files via upload
    server file for brokers

---
+++ raftnode.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port 8080")
+
+	for {
+		// Accept incoming connections
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		// Handle client connection in a goroutine
+		go handleClient(conn)
+	}
+
+}
+
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	// Read and process data from the client
+	// ...
+
+	// Write data back to the client
+	// ...
+}
