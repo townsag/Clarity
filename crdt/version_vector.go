755e7e6dcb620a084824418548f57944329919af
townsag <townsag@gmail.com>
2024-11-17 18:16:25 -0800

    bug fixes and cosmetic changes

--- crdt/version_vector.go
+++ crdt/version_vector.go
@@ -13,7 +13,7 @@ func NewVersionVector(replicaID string) *VersionVector {
 	v := &VersionVector{
 		counters: make(map[string]int64),
 	}
-	v.counters[replicaID] = 0
+	v.counters[replicaID] = 0		// does this mean that the version number / operation offset is 1 indexed
 	return v
 }
 
@@ -46,21 +46,21 @@ func (v *VersionVector) RegisterReplica(replicaID string) error {
 	if _, ok := v.counters[replicaID]; ok {
 		return fmt.Errorf("tried to register new replicaID that already existed in version vector: %v", replicaID)
 	}
-	v.counters[replicaID] = 0
+	v.counters[replicaID] = 0	// this means that the version 
 	return nil
 }
 
-func (v *VersionVector) IsDuplicateOperation(replicaID string, operationOffset int64) bool {
-	localCounter, ok := v.counters[replicaID]
-	if !ok || localCounter < operationOffset {
+func (v *VersionVector) IsDuplicateOperation(operationID ID) bool {
+	localCounter, ok := v.counters[operationID.replicaID]
+	if !ok || localCounter < operationID.operationOffset {
 		return false
 	}
 	return true
 }
 
-func (v *VersionVector) IsValidOperation(replicaID string, operationOffset int64) bool {
-	if current_operation_index, ok := v.counters[replicaID]; ok {
-		return current_operation_index == operationOffset + 1
+func (v *VersionVector) IsValidOperation(operationID ID) bool {
+	if current_operation_index, ok := v.counters[operationID.replicaID]; ok {
+		return current_operation_index + 1 == operationID.operationOffset
 	}
 	return false
 }
@@ -73,16 +73,15 @@ func (v *VersionVector) IncrementVersion(replicaID string) (newOperationOffset i
 	return v.counters[replicaID], nil
 }
 
-func (v *VersionVector) UpdateOperation(replicaID string, operationOffset int64) error {
-	if v.IsValidOperation(replicaID, operationOffset) {
-		v.counters[replicaID] += 1
+func (v *VersionVector) UpdateOperation(operationID ID) error {
+	if v.IsValidOperation(operationID) {
+		v.counters[operationID.replicaID] += 1
 	} else {
 		return fmt.Errorf(
 			"tried to apply operation index %d to operation %d",
-			operationOffset, 
-			v.counters[replicaID],
+			operationID.operationOffset, 
+			v.counters[operationID.replicaID],
 		)
 	}
 	return nil
-
 }
