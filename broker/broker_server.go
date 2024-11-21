8f2a9fe74d0f6fe9684e02f7a26b3b564b996eac
HarrisonHwang <78999886+HarrisonHwang@users.noreply.github.com>
2024-11-21 08:34:50 -0800

    starting on election code
    election relies on most recent log index. we need logs to work first

--- broker/broker_server.go
+++ broker/broker_server.go
@@ -2,8 +2,11 @@ package broker
 
 import (
 	"log"
+	"math/rand"
 	"net"
+	"net/rpc"
 	"sync"
+	"time"
 )
 
 type LogEntry struct {
@@ -17,21 +20,37 @@ type BrokerServer struct {
 
 	brokerid int
 
-	peerIds []int
+	peerIds     []int
+	peerClients map[int]*rpc.Client
 
 	listener net.Listener
 
 	// persistent state on all servers
 	// should be replicated across all brokers
-	term int // what is the current term
-	vote int // who this server voted for
-	log  []LogEntry
+	term     int // what is the current term
+	votedFor int // who this server voted for
+	log      []LogEntry
 
 	// states unique to each server
 	state ServerState
 
+	electionTimer *time.Timer
+
 	// channel to ensure servers start together
 	ready <-chan any
+}
+
+// i think we can just hardcode initialize one server as leader when we start up the cluster?
+// ready <-chan any is for make sure everything starts are the same time when close(ready) in whatever starting the servers
+func NewBrokerServer(brokerid int, peerIds []int, state ServerState, ready <-chan any) *BrokerServer {
+	broker := new(BrokerServer)
+	broker.brokerid = brokerid
+	broker.peerIds = peerIds
+	broker.peerClients = make(map[int]*rpc.Client)
+	broker.state = state
+	broker.ready = ready
+
+	return broker
 }
 
 type ServerState int
@@ -58,18 +77,6 @@ func (s ServerState) String() string {
 	default:
 		panic("unreachable")
 	}
-}
-
-// i think we can just hardcode initialize one server as leader when we start up the cluster?
-// ready <-chan any is for make sure everything starts are the same time when close(ready) in whatever starting the servers
-func NewBrokerServer(brokerid int, peerIds []int, state ServerState, ready <-chan any) *BrokerServer {
-	broker := new(BrokerServer)
-	broker.brokerid = brokerid
-	broker.peerIds = peerIds
-	broker.state = state
-	broker.ready = ready
-
-	return broker
 }
 
 // Broker Server's main routine
@@ -100,7 +107,64 @@ func (broker *BrokerServer) Serve() {
 
 	broker.mu.Unlock()
 
+	// start listening for request
+	go func() {
+		for {
+			conn, err := broker.listener.Accept()
+			if err != nil {
+				log.Fatal(err)
+				continue
+			}
+			// go routine so that rpc is non blocking
+			go rpc.ServeConn(conn)
+
+		}
+	}()
+
+	// start election timer
+	broker.resetElectionTimer()
+
 	// if follower gets a log update. reject? then app server should resend to leader
+}
+
+// set randomized timeout when called
+// or start election when timeout runs out
+func (broker *BrokerServer) resetElectionTimer() {
+	if broker.electionTimer != nil {
+		broker.electionTimer.Stop()
+	}
+
+	timeout := time.Duration(150+rand.Intn(150)) * time.Millisecond
+	broker.electionTimer = time.NewTimer(timeout)
+
+	// start election when timer runs out
+	go func() {
+		<-broker.electionTimer.C
+		broker.startElection()
+	}()
+}
+
+// election algorithm
+func (broker *BrokerServer) startElection() {
+
+	broker.mu.Lock()
+	broker.state = Candidate
+	broker.term++
+	savedCurrentTerm := broker.term
+
+	// server votes for itself
+	//votes := 1
+	log.Printf("becomes Candidate (currentTerm=%d)", savedCurrentTerm)
+	broker.mu.Unlock()
+
+	// send vote request rpc to all peers
+	for _, peerId := range broker.peerIds {
+		go func(peerId int) {
+			// need log index so need logs to work first
+			return
+		}(peerId)
+	}
+
 }
 
 // func main() {
