33a0c12bd0d943675e9fd996dee65d1e3168fa8f
HarrisonHwang <78999886+HarrisonHwang@users.noreply.github.com>
2024-12-05 17:16:30 -0800

    time metrics for appserver

--- appserver/appserver_test.go
+++ appserver/appserver_test.go
@@ -10,12 +10,18 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-func TestHandleWebSocket(t *testing.T) {
+func TestHandleSendAndGet3brokers(t *testing.T) {
+
+	// for metrics
+	teststart := time.Now()
+	createClusterStart := time.Now()
 
 	// use broker server harness to create cluster of brokers
 	h := broker.NewHarness(t, 3)
 
-	// Setup the AppServer
+	createClusterEnd := time.Now()
+
+	// get brokerserver http addresses
 	brokerAddrs := make([]string, len(h.Cluster()))
 	for i, broker := range h.Cluster() {
 		brokerAddrs[i] = broker.GetHTTPAddr()
@@ -50,6 +56,8 @@ func TestHandleWebSocket(t *testing.T) {
 		Source:    "client",
 	}
 
+	sendMessageToBrokerStart := time.Now()
+
 	// send log to brokers
 	err = client.WriteJSON(msg)
 	if err != nil {
@@ -57,19 +65,129 @@ func TestHandleWebSocket(t *testing.T) {
 	}
 	log.Printf("Log %+v sent to brokers", msg)
 
+	sendMessageToBrokerEnd := time.Now()
+
+	requestLogFromBrokerStart := time.Now()
+
 	// Call requestCRDTLogs to get the logs from the brokers
 	if err := appServer.requestCRDTLogs(); err != nil {
 		t.Fatalf("failed to request CRDT logs: %v", err)
 	}
 
+	requestLogFromBrokerEnd := time.Now()
+
 	time.Sleep(1 * time.Second)
 
-	// Verify that the AppServer's CRDT state has the expected update
-	// expectedState := []interface{}{"test"} // The expected value after the insert operation
-	// actualState := appServer.GetRepresentation()
+	testend := time.Now()
 
-	// if len(actualState) != len(expectedState) || actualState[0] != expectedState[0] {
-	// 	t.Fatalf("Expected CRDT state %v, but got %v", expectedState, actualState)
-	// }
+	h.Shutdown()
+
+	// create and print time metrics
+
+	testDuration := testend.Sub(teststart)
+	createClusterDuration := createClusterEnd.Sub(createClusterStart)
+	sendMessageToBrokerDuration := sendMessageToBrokerEnd.Sub(sendMessageToBrokerStart)
+	requestLogFromBrokerDuration := requestLogFromBrokerEnd.Sub(requestLogFromBrokerStart)
+
+	roundtripDuration := requestLogFromBrokerEnd.Sub(sendMessageToBrokerStart)
+
+	log.Printf("\n\n\n\n")
+	log.Printf("[TestHandleSendAndGet3brokers] metrics:")
+	log.Printf("Test took %s", testDuration)
+	log.Printf("cluster creation took: %s", createClusterDuration)
+	log.Printf("appserver send message to broker took: %s", sendMessageToBrokerDuration)
+	log.Printf("appserver request log from broker took: %s", requestLogFromBrokerDuration)
+	log.Printf("roundtrip: %s", roundtripDuration)
+
+}
+
+func TestHandleSendAndGet7brokers(t *testing.T) {
+
+	// for metrics
+	teststart := time.Now()
+	createClusterStart := time.Now()
+
+	// use broker server harness to create cluster of brokers
+	h := broker.NewHarness(t, 7)
+
+	createClusterEnd := time.Now()
+
+	// get brokerserver http addresses
+	brokerAddrs := make([]string, len(h.Cluster()))
+	for i, broker := range h.Cluster() {
+		brokerAddrs[i] = broker.GetHTTPAddr()
+	}
+
+	// set up app server
+	appServer := NewAppServer("testReplica", brokerAddrs)
+	go func() {
+		if err := appServer.Serve(":8080"); err != nil {
+			//t.Fatalf("failed to start AppServer: %v", err)
+		}
+	}()
+
+	// Allow time for servers to start
+	time.Sleep(2 * time.Second)
+
+	// Connect a WebSocket client
+	addr := "ws://localhost:8080/ws"
+	client, _, err := websocket.DefaultDialer.Dial(addr, nil)
+	if err != nil {
+		t.Fatalf("failed to connect to WebSocket server: %v", err)
+	}
+	defer client.Close()
+
+	// Send a test message to the WebSocket server
+	msg := Message{
+		Type:      "insert",
+		Index:     1,
+		Value:     "test",
+		ReplicaID: "testReplica",
+		OpIndex:   1,
+		Source:    "client",
+	}
+
+	sendMessageToBrokerStart := time.Now()
+
+	// send log to brokers
+	err = client.WriteJSON(msg)
+	if err != nil {
+		t.Fatalf("failed to send WebSocket message: %v", err)
+	}
+	log.Printf("Log %+v sent to brokers", msg)
+
+	sendMessageToBrokerEnd := time.Now()
+
+	requestLogFromBrokerStart := time.Now()
+
+	// Call requestCRDTLogs to get the logs from the brokers
+	if err := appServer.requestCRDTLogs(); err != nil {
+		t.Fatalf("failed to request CRDT logs: %v", err)
+	}
+
+	requestLogFromBrokerEnd := time.Now()
+
+	time.Sleep(1 * time.Second)
+
+	testend := time.Now()
+
+	h.Shutdown()
+
+	// create and print time metrics
+
+	testDuration := testend.Sub(teststart)
+	createClusterDuration := createClusterEnd.Sub(createClusterStart)
+	sendMessageToBrokerDuration := sendMessageToBrokerEnd.Sub(sendMessageToBrokerStart)
+	requestLogFromBrokerDuration := requestLogFromBrokerEnd.Sub(requestLogFromBrokerStart)
+
+	roundtripDuration := requestLogFromBrokerEnd.Sub(sendMessageToBrokerStart)
+
+	log.Printf("\n\n\n\n")
+	log.Printf("[TestHandleSendAndGet3brokers] metrics:")
+	log.Printf("Test took %s", testDuration)
+	log.Printf("cluster creation took: %s", createClusterDuration)
+	log.Printf("appserver send message to broker took: %s", sendMessageToBrokerDuration)
+	log.Printf("appserver request log from broker took: %s", requestLogFromBrokerDuration)
+	log.Printf("roundtrip: %s", roundtripDuration)
 
 }
